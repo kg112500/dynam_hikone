8c34f683b9911f38d05d9612d4d45de85d8d0434
kg112500 <k.g.guitar11@gmail.com>
2026-02-22 03:42:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ MAPPING_URL = "https://docs.google.com/spreadsheets/d/1SEDGQLHGRN0rnXgLvP7wNzUuc
 
 # --- ページ設定 ---
 st.set_page_config(page_title="ダイナム彦根分析ツール", layout="wide")
+# st.title の代わりに markdown を使い、文字サイズ(font-size)を指定します
+st.markdown("<h2 style='font-size: 22px; margin-bottom: 0px;'>🎰 ダイナム彦根分析ツール (Pro版)</h2>", unsafe_allow_html=True)
+# h3 (###) よりも少し小さいサイズで指定します
 st.markdown(f"<div style='font-size: 16px; font-weight: bold; margin-top: 15px; margin-bottom: 10px;'>🎯 分析対象: {title_str}</div>", unsafe_allow_html=True)
 
 # --- 1. データ読み込み ---
@@ -483,6 +486,7 @@ with tab4:
                 fig5.update_traces(texttemplate="%{z:.1f}%", hovertemplate="機種: %{y}<br>ゾロ目: %{x}<br>機械割: %{z:.1f}%")
                 st.plotly_chart(fig5, use_container_width=True)
             else: st.info("ゾロ目データなし")
+
 
 
 
